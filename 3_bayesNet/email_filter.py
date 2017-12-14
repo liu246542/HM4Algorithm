29d3d6b32aeb435b3215d1636df2df6dd384e327
Liu <liu246542@163.com>
2017-12-14 16:26:38 +0800

    fix some bugs

--- 3_bayesNet/email_filter.py
+++ 3_bayesNet/email_filter.py
@@ -4,43 +4,34 @@
 import nltk
 from functools import reduce
 
-# 文本处理函数
-# 训练函数
-# 测试函数
-
-# 分词
+# text processing
 def splitWord(listCont):
-	# raw_words = set() # 不重复
-	raw_words = [] # 重复
+	raw_words = []
 	for email in listCont:
 		rule_word = nltk.RegexpTokenizer("[\w']{2,}")
 		raw_lines = rule_word.tokenize(email)
-		raw_lines.pop(0) # 去掉第一个Subject
-
-		# raw_words.update(set(raw_lines)) # 不重复
-		# raw_words.extend(raw_lines) # 重复
-		raw_words.append(raw_lines) # 保证邮件
-	# print(len(raw_words))
+		raw_lines.pop(0) # discard the string 'Subject'
+		raw_words.append(raw_lines)
 	return raw_words
 
-# 构建
+# construct dictionary
 def constDict(listCont):
 	wordDict = {}
 	raw_words = splitWord(listCont)
 	for email in raw_words:
 		for word in email:
-			wordDict.setdefault(word.strip().lower(),[0.5,0.5]) # [垃圾邮件概率，正常邮件概率]
+			wordDict.setdefault(word.strip().lower(),[0.5,0.5]) # [spam，normal]
 	# print(len(wordDict))
 	return wordDict
 
-# 计数
+# count the words frequency
 def count_num(ema_word,sin_word):
 	count = 0
 	for email in ema_word:
 		if sin_word in email: count += 1
 	return count
 
-# 训练
+# train
 def train(spaEmail,norEmail,wordDict):
 	spa_Email = splitWord(spaEmail) # list(list())类型
 	nor_Email = splitWord(norEmail)
@@ -49,36 +40,30 @@ def train(spaEmail,norEmail,wordDict):
 	for sin_word in list(wordDict):
 		num_spa = count_num(spa_Email,sin_word)
 		num_nor = count_num(nor_Email,sin_word)
-		if ((num_spa + num_nor) > (len_smail + len_nmail)) or (num_spa + num_nor < 5):
+		# wordDict[sin_word] = [(num_spa) / (len_smail), (num_nor) / (len_nmail)]
+		# wordDict[sin_word] = [(num_spa + 1) / (num_spa + len_smail), (num_nor + 1) / (num_nor + len_nmail)]
+		if ((num_spa + num_nor) == (len_smail + len_nmail)) or (num_spa + num_nor < 5):
 			wordDict.pop(sin_word)
 		else :
 			wordDict[sin_word] = [(num_spa + 1) / (num_spa + len_smail), (num_nor + 1) / (num_nor + len_nmail)]
 	return wordDict
 
-
-#-- 计算每个单词的条件概率
-#++ 计算每封邮件的概率
+# count the joint probability
 def cotWordPro(ewords,wordDict):
 	pro_res = []
 	for sin_word in ewords:
 		[pws,pwn] = wordDict.get(sin_word,[0.4,0.4])
-		sin_res = pws + pwn
-		# sin_res = 0 if (pws + pwn)==0 else pws+pwn
-		# pws/(pws + pwn)
+		sin_res = pws / (pws + pwn)
 		pro_res.append(sin_res)
-	pro_res.sort(reverse = True)
-	pro_res = pro_res[0:15]
 	pro_mup = reduce(lambda x,y: x*y,pro_res)
 	ipr_res = list(map(lambda x: 1-x,pro_res))
 	ipr_mup = reduce(lambda x,y: x*y,ipr_res)
-	return (pro_mup / (pro_mup + ipr_mup))
+	return 1 if  (pro_mup + ipr_mup) ==0 else pro_mup / (pro_mup + ipr_mup)
 
-# 预测
+# predict
 def predict(email_list,wordDict):
 	email_words = splitWord(email_list)
-	## 计算每个单词的概率
-	## 如果没有的话，就
-	th_value = 0.9
+	th_value = 0.9 # threshold value
 	pre_res = []
 	for ewords in email_words:
 		con_res = 1 if cotWordPro(ewords,wordDict) > th_value else 0
--- 3_bayesNet/test.py
+++ 3_bayesNet/test.py
@@ -6,24 +6,27 @@ from email_filter import constDict,train,predict
 
 if __name__ == '__main__':
 	data_file = csv.reader(open('data/assignment1_data.csv','r'))
-	spamEmail = [] # 垃圾邮件
-	normalEmail = [] # 正常邮件
+	spamEmail = [] # spam emails list
+	normalEmail = [] # normal emails list
 	for item in data_file:
 		if item[1] == '0':
 			normalEmail.append(item[0])
 		elif item[1] == '1':
 			spamEmail.append(item[0])
+	print('---text processing---')
+	wordDict = constDict(normalEmail[0:1000] + spamEmail[0:1000])
+	print('---training----------')
+	wordDict = train(spamEmail[0:1000],normalEmail[0:1000],wordDict)
+	print('---predicting--------')
+	nor_pre_result = predict(normalEmail[4160:4360],wordDict)
+	spa_pre_result = predict(spamEmail[1168:1368],wordDict)
 
-	# print(constDict(normalEmail[0:2]))
-	# splitWord(spamEmail[0:2])
-	wordDict = constDict(normalEmail[0:100] + spamEmail[0:100])
-	wordDict = train(normalEmail[0:100],normalEmail[0:100],wordDict)
-	# pre_result = predict(spamEmail[101:200],wordDict)
-	pre_result = predict(spamEmail[200:300],wordDict)
-	print(len(pre_result))
-	print(pre_result.count(1))
-	print(pre_result.count(1) / len(pre_result))
-	# print(wordDict)
+	a = spa_pre_result.count(1)
+	b = nor_pre_result.count(1)
+	c = spa_pre_result.count(0)
+
+	print('Recall Rate : {:.2}'.format(a/(a + c)))
+	print('Precision Rate: {:.2}'.format(a/(a + b)))
 
 #print(len(healEmail)) # 4360
 #print(len(spamEmail)) # 1368
